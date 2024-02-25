050434213245df4d5b957af95b8e6d629318dffd
Mehul Kumar <97253470+Mehul-Kumar-27@users.noreply.github.com>
2024-02-25 10:39:10 +0000

    enhancements 2

--- build/lib/ui.sh
+++ build/lib/ui.sh
@@ -15,11 +15,17 @@ riotpot::ui::install_node() {
     if ! command -v nvm &> /dev/null; then
         # nvm not found, install it
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
         # Ensure nvm is available in the current session
         export NVM_DIR="/usr/local/share/nvm"
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     fi
+
+    # Load nvm in the current session
+    export NVM_DIR="/usr/local/share/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
     # Install Node.js version 18.18.0 and set it as default
     nvm install 18.18.0
